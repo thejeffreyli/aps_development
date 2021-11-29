5a54c7bb7e8df68d683f12b40d6e23aa67db139a
Jeffrey Li <59130548+thejeffreyli@users.noreply.github.com>
2021-11-29 16:42:53 -0500

    Update rigaku_merged.py

--- bluesky_development/rigaku_merged.py
+++ bluesky_development/rigaku_merged.py
@@ -4,7 +4,7 @@ from ophyd import Component as Cpt
 from bluesky import plan_stubs as bps
 
 # merged class
-class Rigaku500k_Merged(Device):
+class Rigaku500k_Cam1(Device):
     
     # CAM1
     acquire_time = Cpt(EpicsSignal, '8idRigaku:cam1:AcquireTime')
@@ -15,22 +15,72 @@ class Rigaku500k_Merged(Device):
     data_type = Cpt(EpicsSignal, '8idRigaku:cam1:DataType')
     
     det_state = Cpt(EpicsSignal, '8idRigaku:cam1:AcquisitionDelay') 
-    cam_file_name = Cpt(EpicsSignal, '8idRigaku:cam1:FileName')    
+    file_name = Cpt(EpicsSignal, '8idRigaku:cam1:FileName')    
     acquire = Cpt(EpicsSignal, '8idRigaku:cam1:Acquire')  
     num_que_arrays = Cpt(EpicsSignal, '8idRigaku:cam1:NumQueuedArrays') 
 
-
+class Rigaku500k_HDF1(Device):
     # HDF1
     auto_inc = Cpt(EpicsSignal, '8idRigaku:HDF1:AutoIncrement') 
     num_capture = Cpt(EpicsSignal, '8idRigaku:HDF1:NumCapture')
-    hdf_file_name = Cpt(EpicsSignal, '8idRigaku:HDF1:FileName')
+    file_name = Cpt(EpicsSignal, '8idRigaku:HDF1:FileName')
     file_num = Cpt(EpicsSignal, '8idRigaku:HDF1:FileNumber')
     capture = Cpt(EpicsSignal, '8idRigaku:HDF1:Capture_RBV')
 
 
 # object
 class Rigaku500k(Device):
-    cam1 = Cpt(Rigaku500k_Merged)
+    cam1 = Cpt(Rigaku500k_Cam1)
+    hdf1 = Cpt(Rigaku500k_HDF1)
+
+def Rigaku_Slow(rigaku500k):
+    
+    
+    # unix('rm -rf /home/8ididata/RigakuEpics/test*')
+    
+    yield from bps.mv(rigaku500k.cam1.image_mode, "16 Bit, 1S")      
+    yield from bps.mv(rigaku500k.cam1.trigger_mode, "Fixed Time")          
+    yield from bps.mv(rigaku500k.cam1.data_type, "UInt16")          
+    yield from bps.mv(rigaku500k.hdf1.auto_inc, "Yes")          
+    
+    max_repeats=10
+    number_of_frames=10000
+    
+    yield from bps.mv(rigaku500k.cam1.num_images, number_of_frames)          
+    yield from bps.mv(rigaku500k.hdf1.num_capture, number_of_frames)        
+    
+        
+    # Test t0 = 0.001 s
+    acq_t = 0.001
+   
+    yield from bps.mv(rigaku500k.cam1.acquire_time, acq_t)    
+    
+    for ival in range(0,max_repeats):
+
+        _filename= "test_{:03d}ms_{:02d}.bin".format(acq_t*1000,ival)
+        
+        # unix('date')
+            
+        print("Start Rigaku %s\n",_filename)
+        
+        yield from bps.mv(rigaku500k.hdf1.file_name, _filename)
+        yield from bps.mv(rigaku500k.hdf1.file_num, 1)
+        yield from bps.mv(rigaku500k.hdf1.capture, 1)
+        
+        bps.sleep(0.05)
+            
+        if rigaku500k.cam1.det_state == "Idle":
+            yield from bps.mv(rigaku500k.cam1.acquire, 1)  
+            bps.sleep(0.1)
+            
+        while rigaku500k.hdf1.capture != "Done":
+            bps.sleep(0.1)
+        
+        while rigaku500k.cam1.num_que_arrays > 0:        
+            bps.sleep(0.1)
+            
+        # unix('date')            
+        print("\n")  
 
 def Rigaku_Fast(rigaku500k):
     yield from bps.mv(rigaku500k.cam1.acquire_time, 30e-6)    
@@ -55,62 +105,15 @@ def Rigaku_Fast(rigaku500k):
         
             while rigaku500k.cam1.det_state == "Idle":
                 bps.sleep(0.1)
-                yield from bps.mv(rigaku500k.cam1.cam_file_name, _filename)       
+                yield from bps.mv(rigaku500k.cam1.file_name, _filename)       
                 yield from bps.mv(rigaku500k.cam1.acquire, 1)                  
                 continue
         
             while rigaku500k.cam1.det_state != "Idle":
                 bps.sleep(0.1)
-    
-    
-    
-def Rigaku_Slow(rigaku500k):
-    
-    # unix('rm -rf /home/8ididata/RigakuEpics/test*')
-    
-    yield from bps.mv(rigaku500k.cam1.image_mode, "16 Bit, 1S")      
-    yield from bps.mv(rigaku500k.cam1.trigger_mode, "Fixed Time")          
-    yield from bps.mv(rigaku500k.cam1.data_type, "UInt16")          
-    yield from bps.mv(rigaku500k.cam1.auto_inc, "Yes")          
-    
-    max_repeats=10
-    number_of_frames=10000
-    
-    yield from bps.mv(rigaku500k.cam1.num_images, number_of_frames)          
-    yield from bps.mv(rigaku500k.cam1.num_capture, number_of_frames)        
-    
-        
-    # Test t0 = 0.001 s
-    acq_t = 0.001
-   
-    yield from bps.mv(rigaku500k.cam1.acquire_time, acq_t)    
-    
-    for ival in range(0,max_repeats):
 
-        _filename= "test_{:03d}ms_{:02d}.bin".format(acq_t*1000,ival)
-        
-        # unix('date')
-            
-        print("Start Rigaku %s\n",_filename)
-        
-        yield from bps.mv(rigaku500k.cam1.hdf_file_name, _filename)
-        yield from bps.mv(rigaku500k.cam1.file_num, 1)
-        yield from bps.mv(rigaku500k.cam1.capture, 1)
-        
-        bps.sleep(0.05)
-            
-        if rigaku500k.cam1.det_state == "Idle":
-            yield from bps.mv(rigaku500k.cam1.acquire, 1)  
-            bps.sleep(0.1)
-            
-        while rigaku500k.cam1.capture != "Done":
-            bps.sleep(0.1)
-        
-        while rigaku500k.cam1.num_que_arrays > 0:        
-            bps.sleep(0.1)
-            
-        # unix('date')            
-        print("\n")  
+
+
 
 
 def main():
